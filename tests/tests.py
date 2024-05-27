b2bcf6321a6ace4e3438c532c85278d3ce39f33e
sDanielSilva <d.m.silva@ua.pt>
2024-05-27 20:22:54 +0100

    testes unitarios

---
+++ tests/tests.py
@@ -0,0 +1,64 @@
+import unittest
+from ..api.index import app, db, User, Book, Review, UserBook
+from werkzeug.security import generate_password_hash
+
+class TestAPI(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.db = db
+
+    def test_register(self):
+        response = self.app.post('/register', json={'username': 'test', 'password': 'test'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        hashed_password = generate_password_hash('test', method='pbkdf2:sha256')
+        test_user = User(username='test', password=hashed_password)
+        self.db.session.add(test_user)
+        self.db.session.commit()
+
+        response = self.app.post('/login', json={'username': 'test', 'password': 'test'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_books(self):
+        response = self.app.get('/books')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_book(self):
+        test_book = Book(title='Test Book', author='Test Author', isbn='1234567890123')
+        self.db.session.add(test_book)
+        self.db.session.commit()
+
+        response = self.app.get(f'/book/{test_book.id}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_review(self):
+        test_user = User.query.filter_by(username='test').first()
+        test_book = Book.query.filter_by(isbn='1234567890123').first()
+
+        response = self.app.post('/review', json={'book_id': test_book.id, 'user_id': test_user.id, 'review_text': 'Great book!', 'rating': 5})
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_profile(self):
+        test_user = User.query.filter_by(username='test').first()
+
+        response = self.app.get(f'/profile/{test_user.id}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_book(self):
+        test_user = User.query.filter_by(username='test').first()
+        response = self.app.post('/add_book', json={'isbn': '1234567890123', 'user_id': test_user.id})
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_book_reviews(self):
+        test_book = Book.query.filter_by(isbn='1234567890123').first()
+        response = self.app.get(f'/book_reviews/{test_book.isbn}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_books(self):
+        test_user = User.query.filter_by(username='test').first()
+        response = self.app.get(f'/user_books/{test_user.id}')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
