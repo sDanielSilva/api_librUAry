3dd11d2d3bc4e24b32e46e9f8f6b801679d03dd3
Daniel Silva <114096301+sDanielSilva@users.noreply.github.com>
2024-05-27 23:07:11 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -80,6 +80,11 @@ def token_required(f):
 def home():
     return "Welcome to libruary API!"
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
