353452a4ddfc1a214a451235946423c6f16d55e4
Daniel Silva <114096301+sDanielSilva@users.noreply.github.com>
2024-05-29 16:11:39 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,8 +14,8 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 # Configurações do Flask
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
