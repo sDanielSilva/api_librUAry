28d3980013ec59b44099f0801903c8ca8ed187db
Daniel Silva <114096301+sDanielSilva@users.noreply.github.com>
2024-06-04 10:57:58 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -158,7 +158,7 @@ def login():
     # Geração do token JWT
     token = jwt.encode({
         'user_id': user.id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30)
     }, app.config['SECRET_KEY'], algorithm='HS256')
 
     # Inclua o userId na resposta
